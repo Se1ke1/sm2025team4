cd16eb40cff4767568fdbc47d1b575ad28ea46a2
Kimyoujin <rladbwls3212003@naver.com>
2025-07-21 17:48:36 +0900

    LoginController.java 수정 및 로그인 기능 일부 추가

--- src/main/java/edu/sm/sm2025team4/controller/LoginController.java
+++ src/main/java/edu/sm/sm2025team4/controller/LoginController.java
@@ -1,6 +1,9 @@
 package edu.sm.sm2025team4.controller;
 
+import edu.sm.sm2025team4.dto.Cust;
+import edu.sm.sm2025team4.service.CustService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class LoginController {
+
+    final CustService custService;
 
     @RequestMapping("/login")
     public String login(Model model){
@@ -17,7 +23,7 @@ public class LoginController {
         return "index";
     }
 
-
+    /*
     @RequestMapping("/logout")
     public String logout(HttpSession session){
         if (session != null){
@@ -26,24 +32,22 @@ public class LoginController {
         return "index";
     }
 
-
-    /*
     @RequestMapping("/loginimpl")
     public String loginimpl(Model model,
                             @RequestParam("id")  String id,
-                            @RequestParam("pwd") String pwd,
+                            @RequestParam("password") String pwd,
                             HttpSession session) throws Exception {
         log.info("ID:{}, PWD:{}", id, pwd);
 
-        Cust dbCust = null;
-        dbCust = custService.get(id);
+        Cust custDb = null;
+        custDb = custService.get(id);
         String next = "index";
 
-        if(dbCust == null){
+        if(custDb == null){
             model.addAttribute("center","login");
         }else{
-            if(dbCust.getCustPwd().equals(pwd)){
-                session.setAttribute("logincust",dbCust);
+            if(custDb.getCustPwd().equals(pwd)){
+                session.setAttribute("logincust",custDb);
                 next = "redirect:/";
             }else{
                 model.addAttribute("center","login");
