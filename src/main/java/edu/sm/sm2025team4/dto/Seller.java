a80a45c2900ec604a777f67ec16d780ac4fc53ae
Se1Ke1 <thstpgud07202@gmail.com>
2025-07-21 16:25:59 +0900

    2025-07-21, 16:25 DTO들과 MAPPER.XML 일부, 그리고 일부 레포지토리/서비스 예제 추가

---
+++ src/main/java/edu/sm/sm2025team4/dto/Seller.java
@@ -0,0 +1,14 @@
+package edu.sm.sm2025team4.dto;
+
+import java.sql.Timestamp;
+
+public class Seller {
+    private String seller_id;
+    private String seller_pwd;
+    private String seller_name;
+    private String seller_phone;
+    private String seller_address;
+    private String seller_reliable;
+    private Timestamp seller_regdate;
+    private Timestamp seller_update;
+}
