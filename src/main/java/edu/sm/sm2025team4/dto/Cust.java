0be1c0d49657965fd59e2cf5da1a634f41a8b290
Kimyoujin <rladbwls3212003@naver.com>
2025-07-22 15:17:47 +0900

    Acount.jsp 수정

--- src/main/java/edu/sm/sm2025team4/dto/Cust.java
+++ src/main/java/edu/sm/sm2025team4/dto/Cust.java
@@ -1,6 +1,15 @@
 package edu.sm.sm2025team4.dto;
 
+import lombok.*;
+
 import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
 
 public class Cust {
     private String cust_id;
