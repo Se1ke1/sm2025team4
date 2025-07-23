5fcb1505bf876b88e371d87b4d499860fab6b43a
Se1Ke1 <thstpgud07202@gmail.com>
2025-07-23 14:44:17 +0900

    2025-07-23, 14:43 1. ReviewService 트랜잭션 빼먹은거 추가
    2. ProductService 및 연관 파일들
    트랜잭션 빼먹은거 추가 및 외래키(seller_id) 기반 동작 추가
    product와 product_img 간의 동작 일정량 분리

--- src/main/java/edu/sm/sm2025team4/repository/ProductRepository.java
+++ src/main/java/edu/sm/sm2025team4/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package edu.sm.sm2025team4.repository;
 
 import edu.sm.sm2025team4.dto.Product;
+import edu.sm.sm2025team4.frame.ForeignKeyRepository;
 import edu.sm.sm2025team4.frame.SmRepository;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public interface ProductRepository extends SmRepository<Product, Integer> {
+public interface ProductRepository extends SmRepository<Product, Integer>, ForeignKeyRepository<Product, String> {
 }
