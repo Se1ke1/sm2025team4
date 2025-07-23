1ac82127953e05868b985aaccf9deb0d157471d9
Shin Changyoung <toyoaki800@gmail.com>
2025-07-23 17:23:40 +0900

    MainController 코드 수정

--- src/main/java/edu/sm/sm2025team4/controller/MainController.java
+++ src/main/java/edu/sm/sm2025team4/controller/MainController.java
@@ -18,21 +18,5 @@ public class MainController {
         model.addAttribute("center","center");
         return "index";
     }
-    @RequestMapping("/register")
-    public String register(Model model){
-        model.addAttribute("center","register");
-        return "index";
-    }
-    @RequestMapping("/registerimpl")
-    public String registerimpl(Model model, Cust cust, HttpSession session) {
-        try {
-            custService.register(cust);
-            session.setAttribute("cust",cust);
-        }
-        catch (Exception e) {
-//            TODO: 회원가입 실패(DB 접근 문제) 에러로그 전달하기
-            return "redirect:/register";
-        }
-        return "redirect:/";
-    }
+
 }
