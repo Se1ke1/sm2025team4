2e39c1e61a9cc7b0d37fef4918d3394b829bd87f
Se1Ke1 <thstpgud07202@gmail.com>
2025-07-23 16:02:36 +0900

    2025-07-23, 16:00 1. register.jsp 회원가입 스크립트 작성 TODO: 공란/잘못된 비밀번호/아이디 중복 시에 사용자에게 경고문 표시
    2. MainController.java
    registerimpl 작성
    
    3. AjaxController.java
    회원가입 시 아이디 중복 관련 기능 작성

--- src/main/java/edu/sm/sm2025team4/controller/MainController.java
+++ src/main/java/edu/sm/sm2025team4/controller/MainController.java
@@ -1,11 +1,18 @@
 package edu.sm.sm2025team4.controller;
 
+import edu.sm.sm2025team4.dto.Cust;
+import edu.sm.sm2025team4.service.CustService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    final CustService custService;
+
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("center","center");
@@ -15,5 +22,17 @@ public class MainController {
     public String register(Model model){
         model.addAttribute("center","register");
         return "index";
+    }
+    @RequestMapping("/registerimpl")
+    public String registerimpl(Model model, Cust cust, HttpSession session) {
+        try {
+            custService.register(cust);
+            session.setAttribute("cust",cust);
+        }
+        catch (Exception e) {
+//            TODO: 회원가입 실패(DB 접근 문제) 에러로그 전달하기
+            return "redirect:/register";
+        }
+        return "redirect:/";
     }
 }
