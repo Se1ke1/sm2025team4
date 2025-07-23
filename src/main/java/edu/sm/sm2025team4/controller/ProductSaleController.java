48b2e87533048298e39c454f512413934592df62
Shin Changyoung <toyoaki800@gmail.com>
2025-07-23 20:20:32 +0900

    ProductSale.jsp 추가

--- src/main/java/edu/sm/sm2025team4/controller/ProductSaleController.java
+++ src/main/java/edu/sm/sm2025team4/controller/ProductSaleController.java
@@ -1,12 +1,20 @@
 package edu.sm.sm2025team4.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
+@RequiredArgsConstructor
 public class ProductSaleController {
-    @RequestMapping("/procutSale")
-    public String login(Model model){
-        model.addAttribute("center","login");
+
+    String dir = "productSale/";
+
+    @RequestMapping("/productSale")
+    public String procutSale(Model model){
+        model.addAttribute("center", dir+ "productSale");
         return "index";
     }
 
