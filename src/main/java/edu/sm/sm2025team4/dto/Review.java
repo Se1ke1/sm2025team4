3823a74853151fe7f63ba9babf2563becb3943a7
Se1Ke1 <thstpgud07202@gmail.com>
2025-07-23 14:33:39 +0900

    2025-07-23, 14:33 Review와 Review_Img 테이블 관련 파일들 정리 1. 이제 사진 업로드 기능 사용 가능. 2. Review 테이블의 데이터 삭제 시 Review_Img 테이블의 연관 데이터들도 함께 삭제하도록 해놨으나, 사진 추가/삭제와 같은 이미지 제어는 ReviewService가 아니라 Review_ImgService에서 담당할 것.

--- src/main/java/edu/sm/sm2025team4/dto/Review.java
+++ src/main/java/edu/sm/sm2025team4/dto/Review.java
@@ -1,8 +1,10 @@
 package edu.sm.sm2025team4.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,4 +21,6 @@ public class Review {
     private float review_score;
     private Timestamp review_regdate;
     private Timestamp review_update;
+    private List<String> review_img_list;
+    private List<MultipartFile> review_img_file_list;
 }
